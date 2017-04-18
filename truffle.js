a3c5be13eb3a7e010110b0288a2dc6a69efa1d7b
Nicolás Berger <nicoberger@gmail.com>
2017-04-18 10:12:40 -0700

    Run tests with a single command (#13)
    Takes care of running testrpc first, there's no need to have it running
    separately now

--- truffle.js
+++ truffle.js
@@ -1,19 +1,22 @@
+require('babel-register');
+require('babel-polyfill');
+
+var HDWalletProvider = require('truffle-hdwallet-provider');
+
+var mnemonic = '[REDACTED]';
+var provider = new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/');
+
+
 module.exports = {
-  build: {
-    "index.html": "index.html",
-    "app.js": [
-      "javascripts/app.js"
-    ],
-    "app.css": [
-      "stylesheets/app.css"
-    ],
-    "images/": "images/"
-  },
   networks: {
     development: {
-      host: "localhost",
+      host: 'localhost',
       port: 8545,
-      network_id: "*"
+      network_id: '*'
+    },
+    ropsten: {
+      provider: provider,
+      network_id: 3 // official id of the ropsten network
     }
   }
 };
