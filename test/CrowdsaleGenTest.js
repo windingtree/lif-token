6851cb998112d6d5f70bb4e4f551fbdfc2f9e5ed
Nicolas Berger <nicoberger@gmail.com>
2017-07-16 22:03:46 +0200

    Gen test: Add submitBid and setStatus commands. Also refactor
    The refactor is to extract the function that runs the generated crowdsale
    and commands, so we can use it to run a generated example that has failed

--- test/CrowdsaleGenTest.js
+++ test/CrowdsaleGenTest.js
@@ -1,6 +1,7 @@
 var protobuf = require("protobufjs");
 var _ = require('lodash');
 var jsc = require("jsverify");
+var chai = require("chai");
 
 var help = require("./helpers");
 
@@ -28,124 +29,177 @@ contract('LifCrowdsale Property-based test', function(accounts) {
     done();
   });
 
-  it("distributes tokens correctly on any combination of bids", async function() {
-    let crowdsaleRawGen = jsc.record({
-      startPriceEth: jsc.nat,
-      changePerBlock: jsc.nat,
-      changePriceEth: jsc.nat,
-      minCapEth: jsc.nat,
-      maxCapEth: jsc.nat,
-      maxTokens: jsc.nat,
-      presaleBonusRate: jsc.nat,
-      ownerPercentage: jsc.nat
-    });
+  let crowdsaleRawGen = jsc.record({
+    startPriceEth: jsc.nat,
+    changePerBlock: jsc.nat,
+    changePriceEth: jsc.nonshrink(jsc.nat),
+    minCapEth: jsc.nat,
+    maxCapEth: jsc.nat,
+    maxTokens: jsc.nat,
+    presaleBonusRate: jsc.nonshrink(jsc.nat),
+    ownerPercentage: jsc.nonshrink(jsc.nat)
+  });
 
-    let crowdsaleGen = jsc.suchthat(crowdsaleRawGen, function(c) {
-      return (c.maxCapEth >= c.minCapEth) &&
-        (c.changePerBlock > 0);
-    });
+  let crowdsaleGen = jsc.suchthat(crowdsaleRawGen, function(c) {
+    return (c.maxCapEth >= c.minCapEth) &&
+      (c.changePerBlock > 0);
+  });
 
-    let waitBlockCommandGen = jsc.record({
-      type: jsc.constant("waitBlock")
-    });
-    let checkPriceCommandGen = jsc.record({
-      type: jsc.constant("checkPrice")
-    });
+  let waitBlockCommandGen = jsc.record({
+    type: jsc.constant("waitBlock")
+  });
+  let checkPriceCommandGen = jsc.record({
+    type: jsc.constant("checkPrice")
+  });
+  let submitBidCommandGen = jsc.record({
+    type: jsc.constant("submitBid"),
+    account: jsc.elements(accounts),
+    tokens: jsc.nat
+  });
+  let setStatusCommandGen = jsc.record({
+    type: jsc.constant("setStatus"),
+    status: jsc.elements([1,2,3])
+  });
 
-    let commandsGen = jsc.oneof([waitBlockCommandGen, checkPriceCommandGen]);
+  let commandsGen = jsc.nonshrink(jsc.oneof([
+    waitBlockCommandGen,
+    checkPriceCommandGen,
+    submitBidCommandGen,
+    setStatusCommandGen
+  ]));
 
-    let crowdsaleTestInputGen = jsc.record({
-      crowdsale: crowdsaleGen,
-      commands: jsc.array(commandsGen)
-    });
+  let crowdsaleTestInputGen = jsc.record({
+    commands: jsc.array(commandsGen),
+    crowdsale: jsc.nonshrink(crowdsaleGen)
+  });
 
-    let shouldCommandThrow = function(command, state) {
-      if (command.type == "waitBlock") {
-        return false;
-      } else if (command.type = "checkPrice") {
-        let crowdsale = state.crowdsaleData;
-        let { startBlock, endBlock } = crowdsale;
-        return help.shouldCrowdsaleGetPriceThrow(startBlock, endBlock, crowdsale);
-      }
+  let shouldCommandThrow = function(command, state) {
+    if (command.type == "waitBlock") {
+      return false;
+    } else if (command.type == "checkPrice") {
+      let crowdsale = state.crowdsaleData;
+      let { startBlock, endBlock } = crowdsale;
+      return help.shouldCrowdsaleGetPriceThrow(startBlock, endBlock, crowdsale);
+    } else if (command.type == "submitBid") {
+      let crowdsale = state.crowdsaleData;
+      let { startBlock, endBlock } = crowdsale;
+      let price = help.getCrowdsaleExpectedPrice(startBlock, endBlock, crowdsale);
+      let soldTokens = _.sumBy(state.bids || [], (b) => b.tokens);
+
+      return (web3.eth.blockNumber < crowdsale.startBlock) ||
+        (web3.eth.blockNumber > crowdsale.endBlock) ||
+        (state.status != 2) ||
+        (command.tokens == 0) ||
+        (price == 0) ||
+        (soldTokens + command.tokens > crowdsale.maxTokens);
+    } else if (command.type == "setStatus") {
+      return false;
+    } else {
+      assert(false, "unknnow command " + command.type);
     }
+  }
 
-    let runCommand = async function(command, state) {
-      if (command.type == "waitBlock") {
-        await help.waitBlocks(1, accounts);
-        return state;
-      } else if (command.type == "checkPrice") {
-        let expectedPrice = help.getCrowdsaleExpectedPrice(
-          state.crowdsaleData.startBlock, state.crowdsaleData.endBlock, state.crowdsaleData
-        );
-        let price = parseFloat(await state.crowdsaleContract.getPrice());
+  let runCommand = async function(command, state) {
+    if (command.type == "waitBlock") {
+      await help.waitBlocks(1, accounts);
+      return state;
+    } else if (command.type == "checkPrice") {
+      let expectedPrice = help.getCrowdsaleExpectedPrice(
+        state.crowdsaleData.startBlock, state.crowdsaleData.endBlock, state.crowdsaleData
+      );
+      let price = parseFloat(await state.crowdsaleContract.getPrice());
 
-        if (price != 0) {
-          // all of this is because there is a small rounding difference sometimes.
-          let priceDiffPercent = (expectedPrice - price) / price;
-          help.debug("price", price, " expected price: ", expectedPrice, " diff %: ", priceDiffPercent);
-          let maxPriceDiff = 0.000000001;
-          assert.equal(true, Math.abs(priceDiffPercent) <= maxPriceDiff,
-            "price diff should be less than " + maxPriceDiff + " but it's " + priceDiffPercent);
+      if (price != 0) {
+        // all of this is because there is a small rounding difference sometimes.
+        let priceDiffPercent = (expectedPrice - price) / price;
+        help.debug("price", price, " expected price: ", expectedPrice, " diff %: ", priceDiffPercent);
+        let maxPriceDiff = 0.000000001;
+        assert.equal(true, Math.abs(priceDiffPercent) <= maxPriceDiff,
+          "price diff should be less than " + maxPriceDiff + " but it's " + priceDiffPercent);
+      }  else {
+        assert.equal(expectedPrice, price,
+          "expected price is different! Expected: " + expectedPrice + ", actual: " + price + ". blocks: " + web3.eth.blockNumber + ", start/end: " +
+          state.crowdsaleData.startBlock + "/" + state.crowdsaleData.endBlock);
+      }
+
+      return state;
+    } else if (command.type == "submitBid") {
+      let price = help.getCrowdsaleExpectedPrice(
+        state.crowdsaleData.startBlock, state.crowdsaleData.endBlock, state.crowdsaleData
+      );
+      help.debug("submitBid price:", price, "blockNumber:", web3.eth.blockNumber);
+      await state.crowdsaleContract.submitBid({
+        value: price * command.tokens,
+        from: command.account
+      });
+      state.bids = _.concat(state.bids || [], {tokens: command.tokens, price: price, account: command.account});
+      return state;
+    } else if (command.type == "setStatus") {
+      await state.crowdsaleContract.setStatus(command.status, {from: accounts[0]});
+      state.status = command.status;
+      return state;
+    } else {
+      throw("Unknown command type " + command.type);
+    }
+  }
+
+  let runGeneratedCrowdsaleAndCommands = async function(input) {
+    let blocksCount = 5;
+    let startBlock = web3.eth.blockNumber + 5;
+    let endBlock = startBlock + blocksCount;
+
+    help.debug("crowdsaleTestInput data:\n", input, startBlock, endBlock);
+
+    let crowdsaleData = {
+      token: token,
+      startBlock: startBlock, endBlock: endBlock,
+      startPrice: web3.toWei(input.crowdsale.startPriceEth, 'ether'),
+      changePerBlock: input.crowdsale.changePerBlock, changePrice: web3.toWei(input.crowdsale.changePriceEth, 'ether'),
+      minCap: web3.toWei(input.crowdsale.minCapEth, 'ether'), maxCap: web3.toWei(input.crowdsale.maxCapEth, 'ether'),
+      maxTokens: input.crowdsale.maxTokens,
+      presaleBonusRate: input.crowdsale.presaleBonusRate, ownerPercentage: input.crowdsale.ownerPercentage
+    };
+
+    let crowdsale = await help.createCrowdsale(crowdsaleData, accounts);
+
+    help.debug("created crowdsale at address ", crowdsale.address);
+
+    // Assert price == 0 before start
+    let price = parseFloat(await crowdsale.getPrice());
+    assert.equal(price, web3.toWei(0, 'ether'));
+
+    await help.fundCrowdsale(crowdsaleData, crowdsale, accounts);
+
+    var state = {
+      crowdsaleData: crowdsaleData,
+      crowdsaleContract: crowdsale,
+      status: 1 // crowdsale status
+    };
+
+    for (let command of input.commands) {
+      let shouldThrow = shouldCommandThrow(command, state);
+      try {
+        state = await runCommand(command, state);
+        assert.equal(false, shouldThrow, "command " + command + " should have thrown but it didn't.\nState: " + state);
+      }
+      catch(error) {
+        if (error instanceof chai.AssertionError) {
+          throw(error);
         } else {
-          assert.equal(expectedPrice, price,
-            "expected price is different! Expected: " + expectedPrice + ", actual: " + price + ". blocks: " + web3.eth.blockNumber + ", start/end: " + 
-            state.crowdsaleData.startBlock + "/" + state.crowdsaleData.endBlock);
+          assert.equal(true, shouldThrow, "command " + command + " should not have thrown but it did.\nError: " + error + "\nState: " + state);
         }
-
-        return state;
-      } else {
-        throw("Unknown command type " + command.type);
       }
     }
 
-    let property = jsc.forall(crowdsaleTestInputGen, async function(input) {
-      let blocksCount = 5;
-      let startBlock = web3.eth.blockNumber + 5;
-      let endBlock = startBlock + blocksCount;
+    return true;
+  }
 
-      help.debug("crowdsaleTestInput data:\n", input, startBlock, endBlock);
+  it("distributes tokens correctly on any combination of bids", async function() {
+    // stateful prob based tests can take a long time to finish when shrinking...
+    this.timeout(120 * 1000);
 
-      let crowdsaleData = {
-        token: token,
-        startBlock: startBlock, endBlock: endBlock,
-        startPrice: web3.toWei(input.crowdsale.startPriceEth, 'ether'),
-        changePerBlock: input.crowdsale.changePerBlock, changePrice: web3.toWei(input.crowdsale.changePriceEth, 'ether'),
-        minCap: web3.toWei(input.crowdsale.minCapEth, 'ether'), maxCap: web3.toWei(input.crowdsale.maxCapEth, 'ether'),
-        maxTokens: input.crowdsale.maxTokens,
-        presaleBonusRate: input.crowdsale.presaleBonusRate, ownerPercentage: input.crowdsale.ownerPercentage
-      };
-
-      let crowdsale = await help.createCrowdsale(crowdsaleData, accounts);
-
-      help.debug("created crowdsale at address ", crowdsale.address);
-
-      // Assert price == 0 before start
-      let price = parseFloat(await crowdsale.getPrice());
-      assert.equal(price, web3.toWei(0, 'ether'));
-
-      await help.fundCrowdsale(crowdsaleData, crowdsale, accounts);
-
-      var state = {
-        crowdsaleData: crowdsaleData,
-        crowdsaleContract: crowdsale
-      };
-
-      for (let command of input.commands) {
-        let shouldThrow = shouldCommandThrow(command, state);
-        try {
-          state = await runCommand(command, state);
-          assert.equal(false, shouldThrow, "command " + command + " should have thrown but it didn't.\nState: " + state);
-        }
-        catch(error) {
-          if (e instanceof AssertionError) {
-            throw(e);
-          } else {
-            assert.equal(true, shouldThrow, "command " + command + " should not have thrown but it did.\nError: " + error + "\nState: " + state);
-          }
-        }
-      }
-
-      return true;
+    let property = jsc.forall(crowdsaleTestInputGen, async function(crowdsaleAndCommands) {
+      return await runGeneratedCrowdsaleAndCommands(crowdsaleAndCommands);
     });
 
     return jsc.assert(property, {tests: 20});
