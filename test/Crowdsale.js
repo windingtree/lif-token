ad576bc7aacb32879b017c1ce7c124fd1000a7e1
AugustoL <augusto.lemble@gmail.com>
2017-08-10 14:07:58 +0200

    Change test on Crowdsale.js to verify contract vars

--- test/Crowdsale.js
+++ test/Crowdsale.js
@@ -12,25 +12,23 @@ const LOG_EVENTS = true;
 contract('LifToken Crowdsale', function(accounts) {
 
   it("can create a Crowndsale", async function() {
+    const startBlock = web3.eth.blockNumber;
+
     let crowdsale = await LifCrowdsale.new(
-      web3.eth.blockNumber+10, web3.eth.blockNumber+15, web3.eth.blockNumber+20,
+      startBlock+10, startBlock+15, startBlock+20,
       100, 110,
       accounts[0], accounts[1],
       100000000
     );
 
-    console.log(crowdsale.address);
-
-    console.log(await crowdsale.startBlock.call())
-    console.log(await crowdsale.endBlock1.call())
-    console.log(await crowdsale.endBlock2.call())
-    console.log(await crowdsale.rate1.call())
-    console.log(await crowdsale.rate2.call())
-    console.log(await crowdsale.foundationWallet.call())
-    console.log(await crowdsale.marketMaker.call())
-    console.log(await crowdsale.minCap.call())
-
-    assert.equal(web3.eth.blockNumber+10, parseInt(await crowdsale.startBlock.call()));
+    assert.equal(startBlock+10, parseInt(await crowdsale.startBlock.call()));
+    assert.equal(startBlock+15, parseInt(await crowdsale.endBlock1.call()));
+    assert.equal(startBlock+20, parseInt(await crowdsale.endBlock2.call()));
+    assert.equal(100, parseInt(await crowdsale.rate1.call()));
+    assert.equal(110, parseInt(await crowdsale.rate2.call()));
+    assert.equal(accounts[0], parseInt(await crowdsale.foundationWallet.call()));
+    assert.equal(accounts[1], parseInt(await crowdsale.marketMaker.call()));
+    assert.equal(100000000, parseInt(await crowdsale.minCap.call()));
 
   });
 
