0f073c6a2bc29d17568d78eb58f6988c31a5bfc7
Nicolas Berger <nicoberger@gmail.com>
2017-08-14 10:34:20 +0200

    Minor refactor in crowdsale test

--- test/Crowdsale.js
+++ test/Crowdsale.js
@@ -12,18 +12,20 @@ const LOG_EVENTS = true;
 contract('LifToken Crowdsale', function(accounts) {
 
   it("can create a Crowndsale", async function() {
-    const startBlock = web3.eth.blockNumber;
+    const startBlock = web3.eth.blockNumber + 10,
+      endBlock1 = startBlock+15,
+      endBlock2 = startBlock+20;
 
     let crowdsale = await LifCrowdsale.new(
-      startBlock+10, startBlock+15, startBlock+20,
+      startBlock, endBlock1, endBlock2,
       100, 110,
       accounts[0], accounts[1],
       100000000
     );
 
-    assert.equal(startBlock+10, parseInt(await crowdsale.startBlock.call()));
-    assert.equal(startBlock+15, parseInt(await crowdsale.endBlock1.call()));
-    assert.equal(startBlock+20, parseInt(await crowdsale.endBlock2.call()));
+    assert.equal(startBlock, parseInt(await crowdsale.startBlock.call()));
+    assert.equal(endBlock1, parseInt(await crowdsale.endBlock1.call()));
+    assert.equal(endBlock2, parseInt(await crowdsale.endBlock2.call()));
     assert.equal(100, parseInt(await crowdsale.rate1.call()));
     assert.equal(110, parseInt(await crowdsale.rate2.call()));
     assert.equal(accounts[0], parseInt(await crowdsale.foundationWallet.call()));
