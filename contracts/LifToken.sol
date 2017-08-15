1376c96299af451724b49120d4e73903189eb729
AugustoL <augusto.lemble@gmail.com>
2017-08-15 14:11:18 +0200

    Added extra methods on LifToken to transfer data on functions

--- contracts/LifToken.sol
+++ contracts/LifToken.sol
@@ -12,4 +12,54 @@ contract LifToken is MintableToken, Pausable {
 
   // Token decimals
   uint public constant DECIMALS = 18;
+
+  // Edit of the ERC20 token events to support data argument
+  event TransferData(address indexed from, address indexed to, uint value, bytes data);
+  event ApprovalData(address indexed from, address indexed spender, uint value, bytes data);
+
+  //ERC20 token approve method with data call.
+  function approveData(address spender, uint value, bytes data) {
+
+    require(spender != address(this));
+
+    allowed[tx.origin][spender] = value;
+
+    if (spender.call(data))
+      ApprovalData(tx.origin, spender, value, data);
+
+  }
+
+  // ERC20 transfer method with data call.
+  function transferData(address to, uint value, bytes data) {
+
+    require(to != address(this));
+
+    // If transfer have value process it
+    if (value > 0) {
+      balances[tx.origin] = balances[tx.origin].sub(value);
+      balances[to] = balances[to].add(value);
+    }
+
+    if (to.call(data))
+      TransferData(tx.origin, to, value, data);
+
+  }
+
+  // ERC20 transferFrom method with data call.
+  function transferDataFrom(address from, address to, uint value, bytes data) {
+
+    require(to != address(this));
+
+    // If transfer have value process it
+    if (value > 0) {
+      uint allowance = allowed[from][tx.origin];
+      balances[from] = balances[from].sub(value);
+      balances[to] = balances[to].add(value);
+      allowed[from][tx.origin] = allowance.sub(value);
+    }
+
+    if (to.call(data))
+      TransferData(tx.origin, to, value, data);
+
+  }
 }
