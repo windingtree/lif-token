a1accb342e74f36ddf4656b70f0a43ace623d5b8
AugustoL <augusto.lemble@gmail.com>
2017-08-26 12:59:53 +0200

    tests changed to work with crowdsale using caps in USD and creating market maker after succesful corwdsale

--- test/Crowdsale.js
+++ test/Crowdsale.js
@@ -12,30 +12,26 @@ contract('LifToken Crowdsale', function(accounts) {
 
   it("can create a Crowndsale", async function() {
     const publicPresaleStartBlock = web3.eth.blockNumber + 10,
-      endPublicPresale = publicPresaleStartBlock + 10,
-      startBlock = endPublicPresale + 10,
+      publicPresaleEndBlock = publicPresaleStartBlock + 10,
+      startBlock = publicPresaleEndBlock + 10,
       endBlock1 = startBlock + 15,
       endBlock2 = startBlock + 20;
 
     let crowdsale = await LifCrowdsale.new(
-      publicPresaleStartBlock, endPublicPresale,
+      publicPresaleStartBlock, publicPresaleEndBlock,
       startBlock, endBlock1, endBlock2,
       130, 100, 110, 150,
-      accounts[0], accounts[1],
-      100000000,
-      20000000
+      accounts[0]
     );
 
     assert.equal(publicPresaleStartBlock, parseInt(await crowdsale.publicPresaleStartBlock.call()));
-    assert.equal(endPublicPresale, parseInt(await crowdsale.publicPresaleEndBlock.call()));
+    assert.equal(publicPresaleEndBlock, parseInt(await crowdsale.publicPresaleEndBlock.call()));
     assert.equal(startBlock, parseInt(await crowdsale.startBlock.call()));
     assert.equal(endBlock1, parseInt(await crowdsale.endBlock1.call()));
     assert.equal(endBlock2, parseInt(await crowdsale.endBlock2.call()));
     assert.equal(100, parseInt(await crowdsale.rate1.call()));
     assert.equal(110, parseInt(await crowdsale.rate2.call()));
     assert.equal(accounts[0], parseInt(await crowdsale.foundationWallet.call()));
-    assert.equal(accounts[1], parseInt(await crowdsale.marketMaker.call()));
-    assert.equal(100000000, parseInt(await crowdsale.minCap.call()));
 
   });
 
