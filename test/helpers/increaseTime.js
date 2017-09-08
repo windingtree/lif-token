19e879565587cf3747a1f869d0f806c782c0216f
Nicolas Berger <nicoberger@gmail.com>
2017-09-08 19:14:38 -0300

    Resolve eslint issues in time helpers

--- test/helpers/increaseTime.js
+++ test/helpers/increaseTime.js
@@ -3,7 +3,7 @@ var latestTime = require('./latestTime');
 // Increases testrpc time by the passed duration in seconds
 
 function increaseTimeTestRPC(duration) {
-  const id = Date.now()
+  const id = Date.now();
 
   return new Promise((resolve, reject) => {
     web3.currentProvider.sendAsync({
@@ -12,18 +12,18 @@ function increaseTimeTestRPC(duration) {
       params: [duration],
       id: id,
     }, err1 => {
-      if (err1) return reject(err1)
+      if (err1) return reject(err1);
 
       web3.currentProvider.sendAsync({
         jsonrpc: '2.0',
         method: 'evm_mine',
         id: id+1,
       }, (err2, res) => {
-        return err2 ? reject(err2) : resolve(res)
-      })
-    })
-  })
-};
+        return err2 ? reject(err2) : resolve(res);
+      });
+    });
+  });
+}
 
 function increaseTimeTestRPCTo(target) {
   let now = latestTime();
@@ -44,12 +44,12 @@ module.exports = {
   increaseTimeTestRPC: increaseTimeTestRPC,
   increaseTimeTestRPCTo: increaseTimeTestRPCTo,
   duration: {
-    seconds: function(val) { return val},
-    minutes: function(val) { return val * this.seconds(60) },
-    hours:   function(val) { return val * this.minutes(60) },
-    days:    function(val) { return val * this.hours(24) },
-    weeks:   function(val) { return val * this.days(7) },
-    years:   function(val) { return val * this.days(365)}
+    seconds: function(val) { return val; },
+    minutes: function(val) { return val * this.seconds(60); },
+    hours:   function(val) { return val * this.minutes(60); },
+    days:    function(val) { return val * this.hours(24); },
+    weeks:   function(val) { return val * this.days(7); },
+    years:   function(val) { return val * this.days(365); }
   }
 
 }
