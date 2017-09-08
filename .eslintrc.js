8baa5a4ccc07ad42b3246954f7bd336bc6f56792
Nicolas Berger <nicoberger@gmail.com>
2017-09-08 18:30:14 -0300

    Replace jshint with eslint
    It supports async/await by using the parserOption ecmaVersion=8

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 8
+    },
+    "globals": {
+      "assert": true,
+      "web3": true,
+      "artifacts": true
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
