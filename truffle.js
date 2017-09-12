073cdcda651aa8ec26055897c488c241d08ba5f3
AugustoL <augusto.lemble@gmail.com>
2017-09-13 00:48:35 +0200

    fix coverage

--- truffle.js
+++ truffle.js
@@ -23,8 +23,8 @@ module.exports = {
       host: "localhost",
       network_id: "*",
       port: 8555,
-      gas: 0x5B8D80, // 6000000 gas
-      gasPrice: 21000000000 // 21 Gwei
+      gas: 0xfffffffffff,
+      gasPrice: 0x01
     }
   }
 };
