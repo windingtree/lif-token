a6e064c18cbadadfa9c20d030053323dde3e90fa
Nicolas Berger <nicoberger@gmail.com>
2017-09-28 12:32:53 -0300

    Fix transfer and transferFrom test commands: allow 0x0 addresses

--- test/commands.js
+++ test/commands.js
@@ -382,7 +382,7 @@ async function runTransferCommand(command, state) {
     toAddress = gen.getAccount(command.toAccount),
     fromBalance = getBalance(state, command.fromAccount),
     lifWei = help.lif2LifWei(command.lif),
-    hasZeroAddress = _.some([fromAddress, toAddress], isZeroAddress),
+    hasZeroAddress = isZeroAddress(fromAddress),
     shouldThrow = state.tokenPaused || fromBalance.lt(lifWei) ||
       hasZeroAddress;
 
@@ -442,7 +442,7 @@ async function runTransferFromCommand(command, state) {
     toAddress = gen.getAccount(command.toAccount),
     fromBalance = getBalance(state, command.fromAccount),
     lifWei = help.lif2LifWei(command.lif),
-    hasZeroAddress = _.some([senderAddress, toAddress], isZeroAddress),
+    hasZeroAddress = _.some([senderAddress, fromAddress], isZeroAddress),
     allowance = getAllowance(state, command.senderAccount, command.fromAccount);
 
   let shouldThrow = state.tokenPaused ||
