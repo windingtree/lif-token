eab46a9f0be7dda2b6eeb75db87606fdaadccc3a
AugustoL <augusto.lemble@gmail.com>
2018-05-01 10:02:47 +0100

    Fix ci script

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -6,6 +6,13 @@ if [ "$SOLIDITY_COVERAGE" = true ]; then
   yarn run coveralls
 else
   yarn lint
-  WT_DEBUG=true yarn test test/LifToken.js test/Crowdsale.js test/MarketValidationMechanism.js test/VestedPayment.js
-  WT_DEBUG=true GEN_TESTS_TIMEOUT=400 GEN_TESTS_QTY=40 yarn test test/CrowdsaleGenTest.js
+  WT_DEBUG=true yarn test \
+    test/token/LifToken.js \
+    test/token/LifChannels.js \
+    test/token/LifTokenTest.js \
+    test/distribution/Crowdsale.js \
+    test/distribution/MarketValidationMechanism.js \
+    test/distribution/VestedPayment.js \
+    test/proxy/upgradeability.js
+  WT_DEBUG=true GEN_TESTS_TIMEOUT=400 GEN_TESTS_QTY=40 yarn test test/distribution/CrowdsaleGenTest.js
 fi
