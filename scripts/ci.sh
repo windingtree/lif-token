de72af641591463fde4ebb570821693fa9fece22
Hynek Urban <hynek.urban@fragaria.cz>
2018-06-07 08:56:14 +0200

    chore: Replace yarn commands with npm ones.

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,10 +3,11 @@
 set -e
 
 if [ "$SOLIDITY_COVERAGE" = true ]; then
-  yarn run coveralls
+  npm run coveralls
 else
-  yarn lint
-  WT_DEBUG=true yarn test \
+<<<<<<< HEAD
+  npm run lint
+  WT_DEBUG=true npm test \
     test/token/LifToken.js \
     test/token/LifChannels.js \
     test/token/LifTokenTest.js \
@@ -14,5 +15,5 @@ else
     test/distribution/MarketValidationMechanism.js \
     test/distribution/VestedPayment.js \
     test/proxy/upgradeability.js
-  WT_DEBUG=true GEN_TESTS_TIMEOUT=400 GEN_TESTS_QTY=40 yarn test test/distribution/CrowdsaleGenTest.js
+  WT_DEBUG=true GEN_TESTS_TIMEOUT=400 GEN_TESTS_QTY=40 npm test test/distribution/CrowdsaleGenTest.js
 fi
